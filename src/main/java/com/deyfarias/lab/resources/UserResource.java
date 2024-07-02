17e5cf60b1abc7f1e9d555b4960114299aa3cef9
Deywid S. Farias <deywid.farias@gmail.com>
2024-07-02 14:57:58 -0300

    Criacao de entity e resource para a entidade User

---
+++ src/main/java/com/deyfarias/lab/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.deyfarias.lab.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.deyfarias.lab.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@email.com", "(11)98888-7777","123");
+		return ResponseEntity.ok().body(u);
+	}
+}
